514ea4c3347f51417d92c712b7ea5c7ef393e8d9
Martin Pöhlmann <martin.deimos@gmx.de>
2012-06-25 15:33:34 +0200

    First draft of commandline utility. The features just reflect my current needs and are in no way exhaustive.

---
+++ StyleCopExecutor/Main.cs
@@ -0,0 +1,68 @@
+using System;
+using StyleCop;
+using System.Collections.Generic;
+
+namespace StyleCopExecutor
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			string settingsFile = null;
+			string outputFile = null;
+			string workingDirectory = null;
+			List<string> files = new List<string>();
+			
+			string mode = null;
+			foreach(string _arg in args)
+			{
+				var arg = _arg.Trim();
+				if (arg.StartsWith("-"))
+				{
+					mode = arg;
+					continue;
+				}
+				
+				switch(mode)
+				{
+				case "-o":
+					outputFile = arg;
+					break;
+				case "-s":
+					settingsFile = arg;
+					break;
+				case "-w":
+					workingDirectory = arg;
+					break;
+				case "-f":
+					files.Add(arg);
+					break;
+				}
+			}
+			
+			StyleCopConsole console = new StyleCopConsole(
+				settingsFile,
+				false,
+				outputFile,
+				null,
+				true);
+		
+			CodeProject project = new CodeProject(
+				0,
+				System.IO.Directory.GetCurrentDirectory(),
+				new Configuration(null));
+			
+			foreach(string file in files)
+			{
+				var filepath = file;
+				if (workingDirectory != null)
+				{
+					filepath = System.IO.Path.Combine(workingDirectory, file);
+				}
+				console.Core.Environment.AddSourceCode(project, filepath, null);
+			}
+
+			console.Start(new[] { project }, true);
+		}
+	}
+}
